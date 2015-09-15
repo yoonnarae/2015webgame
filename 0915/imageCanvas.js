30dd22efd2f59600234269b588eb023802f7bf76
yoonnarae <7nare7@naver.com>
2015-09-15 20:48:09 +0900

    0915

---
+++ 0915/imageCanvas.js
@@ -0,0 +1,9 @@
+﻿window.addEventListener("load", drawScreen, true);
+
+function drawScreen() {
+	var theCanvas = document.getElementById("GameCanvas");
+	var Context = theCanvas.getContext("2d");
+	//렌더링 컨텍스트 : 2d, 3d
+	Context.fillStyle = "#F9D423";
+	Context.fillRect(0,0,1000,700);
+}
