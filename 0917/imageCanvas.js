6f6e6b9e226778c62707b2920525bac0798ce0fe
yoonnarae <7nare7@naver.com>
2015-09-17 19:30:17 +0900

    0917

---
+++ 0917/imageCanvas.js
@@ -0,0 +1,21 @@
+﻿window.addEventListener("load", drawScreen, true);
+
+function drawScreen() {
+	var theCanvas = document.getElementById("GameCanvas");
+	var Context = theCanvas.getContext("2d");
+	//렌더링 컨텍스트 : 2d, 3d
+	Context.fillStyle = "#F9D423";
+	Context.fillRect(0,0,1000,700);
+	
+	Context.beginPath();
+	Context.moveTo(10,10);
+	Context.lineTo(150,100);
+	Context.strokeStyle="#000";
+	Context.stroke();
+
+	Context.beginPath();
+	Context.arc(200,30,50,1*Math.PI,2*Math.PI,true);
+	//시작점(x,y),반지름,시작각도,끝각도,반시계방향
+	Context.fillStyle="#ff0"
+	Context.fill();
+}
