0822596266b74bca211cf36836306ca43222c6bc
yoonnarae <7nare7@naver.com>
2015-09-17 21:34:38 +0900

    0917

--- 0917/imageCanvas.js
+++ 0917/imageCanvas.js
@@ -4,18 +4,34 @@ function drawScreen() {
 	var theCanvas = document.getElementById("GameCanvas");
 	var Context = theCanvas.getContext("2d");
 	//렌더링 컨텍스트 : 2d, 3d
+	//배경색채우기
 	Context.fillStyle = "#F9D423";
 	Context.fillRect(0,0,1000,700);
 	
+	//선긋기
 	Context.beginPath();
 	Context.moveTo(10,10);
 	Context.lineTo(150,100);
 	Context.strokeStyle="#000";
 	Context.stroke();
 
+	//반원그리기
 	Context.beginPath();
 	Context.arc(200,30,50,1*Math.PI,2*Math.PI,true);
 	//시작점(x,y),반지름,시작각도,끝각도,반시계방향
 	Context.fillStyle="#ff0"
 	Context.fill();
+	
+	//해적기만들기
+	var gradient = Context.createLinearGradient(280,7,80,100);
+	gradient.addColorStop("0","magenta");
+	gradient.addColorStop("0.5","blue");
+	gradient.addColorStop("1.0","red");
+
+	//Fill with gradient
+	Context.strokeStyle = gradient;
+	Context.lineWidth = 5;
+	Context.strokeRect(280,7,80,100);
+
+	
 }
---
+++ 0917/imageCanvas2.js
@@ -0,0 +1,88 @@
+﻿window.addEventListener("load",drawScreen,false);
+window.addEventListener("keydown",onkeydown,false);
+window.addEventListener("keyup",onkeyup,false);
+window.addEventListener("mousemove",onMouseMove,false);
+window.addEventListener("mousedown",onMouseDown,false);
+window.addEventListener("mouseup",onMouseUp,false);
+
+var strKeyEventType = "none";
+var strKeyEventValue = "none";
+
+var bMouseClicked = false;
+intMouseX = 480;
+intMouseY = 300;
+var strMouseStatus = "준비중";
+
+var imgBackground = new Image();
+imgBackground.src = "background.png";
+var imgPlayer = new Image();
+imgPlayer.src = "player.png";
+
+imgPlayer.addEventListener("load",drawScreen,false);
+
+function drawScreen() {
+	var theCanvas = document.getElementById("GameCanvas");
+	var Context = theCanvas.getContext("2d");
+	Context.drawImage(imgBackground,0,0,1000,700);
+	Context.drawImage(imgPlayer,440,200,140,220);
+
+	/*텍스트 그리기
+	Context.fillStyle = "#000";
+	Context.font = "24px nanumgothic";
+	Context.textBaseline = "top";
+	Context.fillText("입력된 키는 : " + strKeyEventValue, 5, 5);
+	Context.fillText("키 입력상태는 : " + strKeyEventType, 5 ,30);
+	*/
+
+	/* 마우스 움직이기 */
+	Context.fillStyle = "#ff0";
+	Context.fillRect(0, 0, 1024, 600);
+	Context.drawImage(imgPlayer, intMouseX, intMouseY, 140, 220);
+	Context.filStyle = "#000";
+	Context.font = "24px nanumgothic";
+	Context.textBaseline - "top";
+	Context.fillText("발생한 마우스 이벤트는 : " + strMouseStatus, 5, 5);
+
+}
+
+function onkeydown(e) {
+	strKeyEventType = e.type;
+	if(e.keyCode)code = e.keyCode;
+	strKeyEventValue = code;
+	drawScreen();
+}
+
+function onkeyup(e) {
+	strKeyEventType = e.type;
+	if(e.keyCode)code = e.keyCode;
+	strKeyEventValue = code;
+	drawScreen();
+}
+
+function onMouseDown(e) {
+	strMouseStatus = "클릭!";
+	var theCanvas = document.getElementById("GameCanvas");
+	bMouseClicked = true;
+	intMouseX = e.clientX - theCanvas.offsetLeft-42;
+	intMouseY = e.clientY - theCanvas.offsetTop-50;
+	drawScreen();
+}
+
+function onMouseMove(e) {
+	strMouseStatus = "Moving now";
+	if(bMouseClicked) {	
+		var theCanvas = document.getElementById("GameCanvas");
+		bMouseClicked = true;
+		intMouseX = e.clientX - theCanvas.offsetLeft-42;
+		intMouseY = e.clientY - theCanvas.offsetTop-50;
+		drawScreen();
+	}
+}	
+
+function onMouseUp(e) {
+	strMouseStatus = "클릭 끝!";
+	bMouseClicked = false;
+	intMouseX = 480;
+	intMouseY = 300;
+	drawScreen();
+}
