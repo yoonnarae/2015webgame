1519885a09247ff9897eb03036827384ea0b0243
yoonnarae <7nare7@naver.com>
2015-09-22 21:09:19 +0900

    0922

--- 0917/imageCanvas2.js
+++ 0917/imageCanvas2.js
@@ -9,39 +9,73 @@ var strKeyEventType = "none";
 var strKeyEventValue = "none";
 
 var bMouseClicked = false;
-intMouseX = 480;
-intMouseY = 300;
+intPlayerX = 480;
+intPlayerY = 300;
 var strMouseStatus = "준비중";
 
 var imgBackground = new Image();
 imgBackground.src = "background.png";
+
 var imgPlayer = new Image();
 imgPlayer.src = "player.png";
 
 imgPlayer.addEventListener("load",drawScreen,false);
 
+var theCanvas;
+
+var Game_STATE_READY = 0;
+var Game_STATE_GAME = 1;
+var Game_STATE_OVER = 2;
+var GameState = Game_STATE_READY;
+
+var ball = new Image();
+ball.src = "ball.png";
+
+var intervalID;
+
+var tempBall1 = { x:0, y:0, go_x:1, go_y:1 };
+var tempBall2 = { x:800, y:0, go_x:-1, go_y:1 };
+var tempBall3 = { x:800, y:600, go_x:-1, go_y:-1 };
+var tempBall4 = { x:0, y:600, go_x:1, go_y:-1 };
+
+
+
 function drawScreen() {
-	var theCanvas = document.getElementById("GameCanvas");
+	theCanvas = document.getElementById("GameCanvas");
 	var Context = theCanvas.getContext("2d");
 	Context.drawImage(imgBackground,0,0,1000,700);
-	Context.drawImage(imgPlayer,440,200,140,220);
+	
+	/* 마우스 움직이기 */
+	Context.drawImage(imgPlayer, intPlayerX, intPlayerY, 140, 220);
+	Context.filStyle = "#000";
+	Context.font = "24px nanumgothic";
+	Context.textBaseline = "top";
+	Context.textAlign = "left";
+	Context.fillText("발생한 마우스 이벤트는 : " + strMouseStatus, 5, 5);
 
-	/*텍스트 그리기
+	/*텍스트 그리기 */
 	Context.fillStyle = "#000";
 	Context.font = "24px nanumgothic";
 	Context.textBaseline = "top";
-	Context.fillText("입력된 키는 : " + strKeyEventValue, 5, 5);
-	Context.fillText("키 입력상태는 : " + strKeyEventType, 5 ,30);
-	*/
+	Context.textAlign = "right";
+	Context.fillText("입력된 키는 : " + strKeyEventValue, 1000, 5);
+	Context.fillText("키 입력상태는 : " + strKeyEventType,  1000, 30);
 
-	/* 마우스 움직이기 */
-	Context.fillStyle = "#ff0";
-	Context.fillRect(0, 0, 1024, 600);
-	Context.drawImage(imgPlayer, intMouseX, intMouseY, 140, 220);
-	Context.filStyle = "#000";
-	Context.font = "24px nanumgothic";
-	Context.textBaseline - "top";
-	Context.fillText("발생한 마우스 이벤트는 : " + strMouseStatus, 5, 5);
+	/* 게임 상태 */
+	if(GameState == Game_STATE_READY) {
+		Context.fillText ("Ready!", 470, 250);
+	} else if(GameState == Game_STATE_GAME) {
+		Context.fillText ("Go!", 300, 200);
+		Context.drawImage(ball, tempBall1.x, tempBall1.y);
+		Context.drawImage(ball, tempBall2.x, tempBall2.y);
+		Context.drawImage(ball, tempBall3.x, tempBall3.y);
+		Context.drawImage(ball, tempBall4.x, tempBall4.y);
+	} else if(GameState == Game_STATE_READY) {
+		Context.font = '60px NanumGothic';
+		Context.fillText ("Game Over", 400, 300);
+	}
+
+
 
 }
 
@@ -49,6 +83,42 @@ function onkeydown(e) {
 	strKeyEventType = e.type;
 	if(e.keyCode)code = e.keyCode;
 	strKeyEventValue = code;
+	/* 게임 상태 */
+	if(GameState == Game_STATE_READY) {
+		if(e.keyCode == 13) {
+			GameState = Game_STATE_GAME;
+			onGameStart();
+		}
+	}else if(GameState == Game_STATE_GAME) {
+		/* 키보드 움직이기 */
+		switch(e.keyCode) {
+		case 37 : 
+			intPlayerX -= 10;
+			if(intPlayerX < 0) 
+				intPlayerX = 0;
+			break;
+		case 39 : 
+			intPlayerX += 10;
+			if(intPlayerX > theCanvas.width-140) 				
+				intPlayerX = theCanvas.width-140;
+			break;
+		case 38 : 
+			intPlayerY -= 10;
+			if(intPlayerY < 0) 
+				intPlayerY = 0;
+			break;
+		case 40 : 
+			intPlayerY += 10;
+			if(intPlayerY > theCanvas.height-220) 
+				intPlayerY = theCanvas.height-220;			
+			break;
+		}
+	}else if(GameState == Game_STATE_OVER) {
+		if(e.keyCode == 13) {
+			GameState = Game_STATE_READY;
+		}
+	}
+	
 	drawScreen();
 }
 
@@ -84,5 +154,25 @@ function onMouseUp(e) {
 	bMouseClicked = false;
 	intMouseX = 480;
 	intMouseY = 300;
+	drawScreen();
+}
+
+function onGameStart() {
+	intervalID = setInterval(MoveBall, 100);
+}
+
+function MoveBall() {
+	tempBall1.x += tempBall1.go_x * 10;
+	tempBall1.y += tempBall1.go_y * 10;
+
+	tempBall2.x += tempBall2.go_x * 10;
+	tempBall2.y += tempBall2.go_y * 10;
+
+	tempBall3.x += tempBall3.go_x * 10;
+	tempBall3.y += tempBall3.go_y * 10;
+
+	tempBall4.x += tempBall4.go_x * 10;
+	tempBall4.y += tempBall4.go_y * 10;
+	
 	drawScreen();
 }
